a4d47eb1c376e4a6c95c98bfa93502fcc0390eb6
AnastasiyaSofina <anastasiyasofina@gmail.com>
2020-06-14 21:21:39 +0300

    HMR

--- Desktop/hometask3/hometask3/webpack.config.js
+++ Desktop/hometask3/hometask3/webpack.config.js
@@ -3,12 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js',
     },
+    devtool: 'inline-source-map',
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
         new HtmlWebpackPlugin({
             title: 'Output Management',
         }),
@@ -16,6 +20,13 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        hot:true
+
     },
     module: {
         rules: [
