7ba8a12bf18d23db772537dd569c2df79b422be9
AnastasiyaSofina <anastasiyasofina@gmail.com>
2020-06-14 20:54:07 +0300

    html using plugin, clearing dist folder

--- Desktop/hometask3/hometask3/src/index.js
+++ Desktop/hometask3/hometask3/src/index.js
@@ -1,14 +1,18 @@
 import _ from 'lodash'
-
 import './style.css';
-
 import Icon from './monkey.svg';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     const myIcon = new Image();
     myIcon.src = Icon;
