60bd10cde82306f129ff957a3a485396bdc48a75
AnastasiyaSofina <anastasiyasofina@gmail.com>
2020-06-14 20:37:45 +0300

    asset setup

--- Desktop/hometask3/hometask3/webpack.config.js
+++ Desktop/hometask3/hometask3/webpack.config.js
@@ -3,7 +3,25 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'), 
-    }
-  };
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+};
+
